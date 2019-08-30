8b703a4bc9a73225dd811ef62d9a0cd9f7729d54
hanyun <1355081829@qq.com>
2019-08-30 11:06:45 +0800

    不要直接组合gorm.Model 这样会出现null

--- main.go
+++ main.go
@@ -6,7 +6,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -16,7 +15,6 @@ import (
 	"qianshen/models"
 	"qianshen/pkg/setting"
 	"qianshen/routers"
-	"time"
 )
 
 func init() {
@@ -47,11 +45,11 @@ func main() {
 
 	log.Println("Shutdown Server ...")
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := s.Shutdown(ctx); err != nil {
-		log.Fatal("Server Shutdown:", err)
-	}
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//if err := s.Shutdown(ctx); err != nil {
+	//	log.Fatal("Server Shutdown:", err)
+	//}
 
 	log.Println("Server exiting")
 }
--- models/user.go
+++ models/user.go
@@ -1,9 +1,7 @@
 package models
 
-import "github.com/jinzhu/gorm"
-
 type User struct {
-	gorm.Model
+	Model
 	UserName string `json:"userName" gorm:"user_name" form:"userName" validate:"required,gt=2"`
 	Password string `json:"password" gorm:"password" form:"password" validate:"required,gt=5,lt=32"`
 	Ip       string `json:"ip" gorm:"ip"`
